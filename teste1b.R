f118fa2cbcbe67ac4986365a43c2d49b4945eaab
Grupo 05 - Renann Camargo Alves <renanncamargo@gmail.com>
2019-02-24 16:06:29 -0300

    Versao 01 - Exercicios

---
+++ teste1b.R
@@ -0,0 +1,84 @@
+########################################
+# Teste 1B          
+# Nome(s): 
+########################################
+
+#ATENÇÃO: Você precisa fazer o download do arquivo chustomer_churn.csv e
+#           deixá-lo na mesma pasta que o arquivo teste1b.R
+#         Depois, Fornecer o caminho completo até o arquivo csv.
+#         Exemplos:
+#              -Windows:
+#                  "C:/Users/Andre/Downloads/customer_churn.csv"
+#              -Ubuntu
+#                  "/home/andre/Downloads/customer_churn.csv"
+#              -Mac OS
+#                  "/Users/andre/Downloads/customer_churn.csv"
+
+customer_churn = read.table("customer_churn.csv", sep=",", header = TRUE, stringsAsFactors= FALSE)
+
+########################################
+# Item 1 (0.5 ponto)
+########################################
+
+########################################
+# Item 2 (0.5 ponto)
+########################################
+
+########################################
+# Item 3 (0.5 ponto)
+########################################
+
+#max_tenure <- #salvar resultado nessa variável
+
+########################################
+# Item 4 (1.0 ponto)
+########################################
+
+#max_tenure_50 <- #salvar resultado nessa variável
+
+########################################
+# Item 5 (1.0 ponto)
+########################################
+
+#min_tenure_mtm <- #salvar resultado nessa variável
+
+########################################
+# Item 6a (1.0 ponto)
+########################################
+
+#total_mtm <- #salvar resultado nessa variável
+#total_year <- #salvar resultado nessa variável
+#total_two_year <- #salvar resultado nessa variável
+
+########################################
+# Item 6b (0.5 ponto)
+########################################
+
+#regular_customers <- #salvar resultado nessa variável
+
+
+########################################
+# Item 7a (0.5 ponto)
+########################################
+
+#customers_with_dependents <- #salvar resultado nessa variável
+
+########################################
+# Item 7b (0.5 ponto)
+########################################
+
+#customers_mtm <- #salvar resultado nessa variável
+#customers_year <- #salvar resultado nessa variável
+#customers_two_year <- #salvar resultado nessa variável
+
+########################################
+# Item 7c (0.5 ponto)
+########################################
+
+#customers_two_years <- #salvar resultado nessa variável
+
+########################################
+# Item 7d (0.5 ponto)
+########################################
+
+#accumulated_discount <- #salvar resultado nessa variável
